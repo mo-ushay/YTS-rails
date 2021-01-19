92d3697bb464fa04209e8ecfd1acd047a794efa7
mo-ushay <muhammad.ushay@sendoso.com>
2021-01-19 17:36:01 +0500

    creating home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+
+  root 'home#index'
+
+  get '/', to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
