01b51c6bfb53b6758596996dcd9b1922b903fed5
mo-ushay <muhammad.ushay@sendoso.com>
2021-01-20 15:45:15 +0500

    correction (movie_role)

--- app/models/movie_role.rb
+++ app/models/movie_role.rb
@@ -3,9 +3,9 @@ class MovieRole < ApplicationRecord
   belongs_to :actor
   roles = Array.new('actor', 'director')
 
-  validates :acting_as, presence: true, inclusion: {
+  validates :acting_as, presence: true
+  validates :role_played, presence: true, inclusion: {
     in: roles,
     message: '%<value> is not a correct role, Please enter again!'
   }
-  validates :role_played, presence: true
 end
