458ed8560f13e7958b28b7b82d1bd71a2a78c515
mo-ushay <muhammad.ushay@sendoso.com>
2021-01-20 15:14:47 +0500

    adeed seeds (actors)

---
+++ db/seeds/actors.rb
@@ -0,0 +1,4 @@
+Actor.where(name: 'Ushay').find_or_create_by!(name: 'Ushay')
+Actor.where(name: 'Jason Statham').find_or_create_by!(name: 'Jason Statham')
+Actor.where(name: 'Gal Gaddot').find_or_create_by!(name: 'Gal Gaddot')
+Actor.where(name: 'Liam Neeson').find_or_create_by!(name: 'Liam Neeson')
