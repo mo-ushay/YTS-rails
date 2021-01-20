6f0f396b480d7d52b99fbc98d296ffb1859b9bf2
mo-ushay <muhammad.ushay@sendoso.com>
2021-01-20 15:42:59 +0500

    adding roles

--- app/models/movie_role.rb
+++ app/models/movie_role.rb
@@ -1,7 +1,11 @@
 class MovieRole < ApplicationRecord
   belongs_to :movie
   belongs_to :actor
+  roles = Array.new('actor', 'director')
 
-  validates :acting_as, presence: true
+  validates :acting_as, presence: true, inclusion: {
+    in: roles,
+    message: '%<value> is not a correct role, Please enter again!'
+  }
   validates :role_played, presence: true
 end
