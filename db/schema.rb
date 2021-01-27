cf2474976d2a89d3f1f2d7cc6ef5b4079714c995
mo-ushay <muhammad.ushay@sendoso.com>
2021-01-27 14:48:17 +0500

    updated "upload movies" form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_112641) do
+ActiveRecord::Schema.define(version: 2021_01_26_174400) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_112641) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "imageable_type"
     t.integer "imageable_id"
+    t.string "path"
     t.index ["imageable_type", "imageable_id"], name: "index_images_on_imageable"
   end
 
