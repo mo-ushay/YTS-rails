d4540501553c457a0b24e33d4ac4e568b6d64c7a
mo-ushay <muhammad.ushay@sendoso.com>
2021-02-11 12:49:08 +0500

    correction*

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,8 +6,8 @@ module Types
     field :name, String, null: false
     field :user_name, String, null: false
     field :introduction, String, null: false
-    field :abilities, String, null: false
-    field :email, [String], null: false
+    field :abilities, [String], null: false
+    field :email, String, null: false
     field :created_at, String, null: false
   end
 end
